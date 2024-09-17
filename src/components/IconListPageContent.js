22a9cb5bc526a1e596962ad9d80a2cd7b3a15185
Arpita Gupta <arpita@copods.co>
2024-09-17 14:57:55 +0530

    fix: image

--- src/components/IconListPageContent.js
+++ src/components/IconListPageContent.js
@@ -15,11 +15,6 @@ const IconListPageContent = () => {
               target: { tabId: activeTab.id },
               func: () => {
                 const svgs = Array.from(document.querySelectorAll("svg"));
-                svgs.forEach((svg) => {
-                  svg.setAttribute("width", "40px");
-                  svg.setAttribute("height", "40px");
-                  svg.style.cursor = "pointer";
-                });
 
                 return svgs.map((svg) => svg.outerHTML);
               },
@@ -32,31 +27,38 @@ const IconListPageContent = () => {
           );
         }
       });
+      if (svgElements) {
+        svgElements.forEach((svg) => {
+          svg.setAttribute("width", "40px");
+          svg.setAttribute("height", "40px");
+          svg.style.cursor = "pointer";
+        });
+      }
     } else {
       console.warn("Chrome APIs are not available in this environment");
     }
+    return () => {
+      setSvgElements([]);
+    };
   }, []);
 
   const sanitizeSVG = (svgString) => {
-    // Create a temporary DOM element to manipulate the SVG
     const tempDiv = document.createElement("div");
     tempDiv.innerHTML = svgString;
 
-    // Select the SVG element
     const svgElement = tempDiv.querySelector("svg");
 
-    // Remove unwanted attributes (class, aria-hidden, style)
     svgElement.removeAttribute("class");
     svgElement.removeAttribute("aria-hidden");
     svgElement.removeAttribute("style");
 
-    return tempDiv.innerHTML; // Return cleaned SVG
+    return tempDiv.innerHTML;
   };
 
   const downloadSVG = (svg, index) => {
     if (svgRef.current) {
       const rawSVG = svgRef.current.innerHTML;
-      const sanitizedSVG = sanitizeSVG(svg); // Clean up the SVG
+      const sanitizedSVG = sanitizeSVG(svg);
 
       const blob = new Blob([sanitizedSVG], { type: "image/svg+xml" });
       const url = URL.createObjectURL(blob);
